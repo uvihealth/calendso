51b0ddf9a72c4ee3113f64a92703793990971a4f
vicke4 <ganapathy.v2@gmail.com>
2021-10-05 20:13:42 +0530

    team event update fix

--- pages/api/availability/eventtype.ts
+++ pages/api/availability/eventtype.ts
@@ -124,7 +124,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       if (req.body.users) {
         data.users = {
           set: [],
-          connect: req.body.users.map((id: number) => ({ id })),
+          connect: req.body.users.map((id: number) => ({ id: parseInt(id) })),
         };
       }
 
